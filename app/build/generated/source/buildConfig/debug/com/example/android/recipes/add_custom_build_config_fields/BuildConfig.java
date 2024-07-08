f63ff2a79e5795995639e3562f5df11be4976ab3
rbozburun <resul.bozburun00@gmail.com>
2024-07-08 15:46:07 +0300

    test

---
+++ app/build/generated/source/buildConfig/debug/com/example/android/recipes/add_custom_build_config_fields/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.android.recipes.add_custom_build_config_fields;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.android.recipes.add_custom_build_config_fields";
+  public static final String BUILD_TYPE = "debug";
+  // Float Value
+  public static final Float FloatValue = 1.0f;
+  // Git Version
+  public static final String GitVersion = "1234";
+}
